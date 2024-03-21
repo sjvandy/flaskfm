035da63ce4f2f712b91c96d1e9aa71efbb74fe64
Steven Vandegrift <svandegrift@icloud.com>
2024-03-20 23:57:33 -0700

    Add Home Route that returns greeting
    Setup basic functionality for Flask Template

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///song_library.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET KEY'] = 'all-hail-the-magic-conch'
+
+db = SQLAlchemy
+
+@app.route('/')
+@app.route('/index')
+def greeting():
+    return render_template('greeting.html')
